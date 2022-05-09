c4fd23fe65d2273c060f08a05b139e6d02837b71
DavidHendrixSantana <dasalvasuper@gmail.com>
2022-05-09 16:53:01 -0500

    Front End: Login and axios install

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import {createStore} from "vuex"
+import axiosClient from "../axios";
 const store = createStore({
     state: {
         user:{
@@ -9,21 +11,20 @@ const store = createStore({
     getters: {},
     actions: {
         register({commit}, user){
-            return fetch('http://localhost:8000/api/register', {
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                method: 'POST',
-                body: JSON.stringify(user),
-            })
-            .then((res) => res.json())
-            .then((res) => {
-                commit('setUser', res);
-                return res
+            return axiosClient.post('/register', user)
+            .then (({data})=>{
+                commit('setUser', data)
+                return data
             })
 
-        }
+        },
+        login({commit}, user){
+            return axiosClient.post('/login', user)
+            .then (({data})=>{
+                commit('setUser', data)
+                return data
+            })
+        },
     },
     mutations : {
         logout: (state) => {
