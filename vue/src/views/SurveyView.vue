0e95bf8c3a4652068e804c3a124b184675a52bc2
DavidHendrixSantana <dasalvasuper@gmail.com>
2022-06-09 13:25:21 -0500

    Add images to Survey

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -57,6 +57,7 @@
                             focus:ring-offset-2
                             focus:ring-indigo-500">
                             <input type="file"
+                            @change="onImageChoose"
                         class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer">
                             Change
                         </button>
@@ -174,6 +175,19 @@ if(route.params.id){
     model.value = store.state.surveys.find(
         (s) => s.id === parseInt(route.params.id)
     )
+}
+
+function onImageChoose(ev) {
+  const file = ev.target.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    // The field to send on backend and apply validations
+    model.value.image = reader.result;
+    // The field to display here
+    model.value.image_url = reader.result;
+    ev.target.value = "";
+  };
+  reader.readAsDataURL(file);
 }
 
 function addQuestion(index){
